4d46c0f02494826da94d4d34f7aa773d7bd2d821
Sauyeed <sauyeed@MacBook-Pro.cust.communityfibre.co.uk>
2020-10-07 10:14:32 +0100

    connected to db and added colors

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 // ES Modules in Node.js - v14.6+
 import express from 'express';
 import dotenv from 'dotenv';
+import colors from 'colors';
+import connectDB from './config/db.js';
 import products from './data/products.js';
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -24,6 +28,6 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT} `
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
   );
 });
