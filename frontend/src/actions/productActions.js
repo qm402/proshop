d6d15f82773aa0552e20154f33e17c3ea8558085
Sauyeed <sauyeed@MacBook-Pro.cust.communityfibre.co.uk>
2020-10-08 16:55:36 +0100

    add dispatch to home screen

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -5,13 +5,13 @@ import {
   PRODUCT_LIST_FAIL,
 } from '../constants/productConstants';
 
-export const listProduct = () => async (dispatch) => {
+export const listProducts = () => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_LIST_REQUEST });
 
     const { data } = await axios.get('/api/products');
 
-    dispatch({ type: PRODUCT_LIST_REQUEST, payload: data });
+    dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
   } catch (error) {
     dispatch({
       type: PRODUCT_LIST_FAIL,
