26e95538706a13a075dc1c502d96cd61873eb296
Sauyeed <sauyeed@MacBook-Pro.cust.communityfibre.co.uk>
2020-12-22 15:07:19 +0000

    back end user authentication

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,40 @@
+import jwt from 'jsonwebtoken';
+import asyncHandler from 'express-async-handler';
+import User from '../models/userModel.js';
+
+// Protect (Auth) Middleware. Protecting routes from being public. Users msut be logged in
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  // console.log(req.headers.authorization);
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    try {
+      // Getting the token and not 'Bearer' too
+      token = req.headers.authorization.split(' ')[1];
+
+      // Verifying token with secret token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      // Putting the user details into req.user
+      req.user = await await User.findById(decoded.id).select('-password');
+      //console.log(req.user);
+
+      // Move onto next middleware
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401);
+      throw new Error('Not authorised, token failed');
+    }
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error('Not authorised, No token');
+  }
+});
+
+export { protect };
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import becrypt from 'bcryptjs';
 
 const userSchema = mongoose.Schema(
   {
@@ -25,6 +26,23 @@ const userSchema = mongoose.Schema(
     timestamps: true,
   }
 );
+
+// Encyrpt password before save to db
+userSchema.pre('save', async function (next) {
+  // Prevent password from being created again
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  const salt = await becrypt.genSalt(10);
+  this.password = await becrypt.hash(this.password, salt);
+});
+
+// Comparing the entereted password with the encrypted password. Return true if match.
+// Can be moved into the user controller too
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await becrypt.compare(enteredPassword, this.password);
+};
 
 const User = mongoose.model('User', userSchema);
 
