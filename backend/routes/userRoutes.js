a3e3876092a784ef6ce8a07343d9680d6d5dc26c
Sauyeed <sauyeed@MacBook-Pro.cust.communityfibre.co.uk>
2021-01-20 12:06:08 +0000

    update user profile route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,12 +4,16 @@ import {
   authUser,
   getUserProfile,
   registerUser,
+  updateUserProfile,
 } from '../controllers/userController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 // All routes have api/users/ before hand
 router.route('/').post(registerUser);
 router.post('/login', authUser);
-router.route('/profile').get(protect, getUserProfile);
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
 export default router;
